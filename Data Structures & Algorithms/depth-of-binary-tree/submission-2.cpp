c6eb16fbc6c19d213329c1bbdad44497ef7ba62f
chandwaninaman2108-prog <chandwaninaman2108@gmail.com>
2026-09-13 00:18:45 +0530

    Add: depth-of-binary-tree - submission-2

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-2.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int level_o_count(TreeNode* root){
+        if(root==NULL) return 0;
+        int ct=1;
+        queue<TreeNode*> q;
+        q.push(root);
+        q.push(NULL);
+        while(!q.empty()){
+            TreeNode* curr=q.front();
+            q.pop();
+            if(curr==NULL){
+                if(!q.empty()){ct++; q.push(NULL); continue;}
+                else{break;}
+            }
+            if(curr->left!=NULL) q.push(curr->left);
+            if(curr->right!=NULL) q.push(curr->right);
+        }
+        return ct;
+    }
+    int maxDepth(TreeNode* root) {
+        return level_o_count(root);
+    }
+};
